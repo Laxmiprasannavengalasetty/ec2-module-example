afb1ff17995553f63fb624d18e1ad2a7ab60eaad
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-09-23 23:44:13 +1000

    tf-module

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 module "ec2"{
 
-    source = "../terraform-ec2-module"
+    source = "git::https://github.com/Laxmiprasannavengalasetty/terraform-ec2-module"   # here source can directly provide git url or ../terraform-ec2-module
     ami_id = var.ami-something
     instance_type = "t3.small"
     vpc_security_group_ids = ["sg-026ccdb1c65b84e63"]
